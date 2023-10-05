9858ecd0981c5a9ed2b2a10afc074580d3406a2d
Tuyen Pham <tuyenbeat@gmail.com>
2023-10-05 16:25:54 +0700

    intialzation project

--- lib/main.dart
+++ lib/main.dart
@@ -7,205 +7,64 @@ import 'package:flame/game.dart';
 import 'package:flame/input.dart';
 import 'package:flame/palette.dart';
 import 'package:flame_audio/flame_audio.dart';
+import 'package:flame_spine/flame_spine.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 import 'package:flutter/src/services/keyboard_key.g.dart';
 
-enum PlayerState { jump }
+enum PlayerState { running, jump }
 
 enum MoveDirection { left, right, up, down }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initSpineFlutter();
+  runApp(GameWidget(game: FlameSpineExample()));
+}
 
-main() {
-  runApp(
-    GameWidget(
-      game: GameCtrl(),
-    ),
+class FlameSpineExample extends FlameGame with TapDetector, KeyboardEvents {
+ late final SpineComponent spineboy;
+
+ @override
+ Future<void> onLoad() async {
+  await initSpineFlutter();
+  // Load the Spineboy atlas and skeleton data from asset files
+  // and create a SpineComponent from them, scaled down and
+  // centered on the screen
+  spineboy = await SpineComponent.fromAssets(
+   atlasFile: 'assets/images/spineboy.atlas',
+   skeletonFile: 'assets/images/spineboy-pro.json',
+   scale: Vector2(0.4, 0.4),
+   anchor: Anchor.center,
+   position: size / 2,
   );
-}
 
-class GameCtrl extends FlameGame with HasKeyboardHandlerComponents {
-  bool pause = false;
-  late CameraComponent cam;
-  @override
-  FutureOr<void> onLoad() {
-    Map lv1 = Map();
-    cam = CameraComponent.withFixedResolution(
-        world: lv1, width: 720, height: 400);
-    cam.viewfinder.anchor = Anchor.topLeft;
-    addAll([cam, lv1]);
-    return super.onLoad();
-  }
-}
+  // Set the "walk" animation on track 0 in looping mode
+  spineboy.animationState.setAnimationByName(0, 'run-to-idle', true);
+  await add(spineboy);
+ }
 
-class Map extends World {
-  final List<Block> _blockList = [];
-  late Player _player;
-
-  @override
-  FutureOr<void> onLoad() async {
-    TiledComponent map = await TiledComponent.load("lv1.tmx", Vector2.all(16));
-    final spawnCharacter = map.tileMap.getLayer<ObjectGroup>("Spawn");
-    if (spawnCharacter is ObjectGroup) {
-      for (final spawn in spawnCharacter.objects) {
-        switch (spawn.class_) {
-          case "Player":
-            _player = Player(position: Vector2(spawn.x, spawn.y));
-            add(_player);
-            break;
-          case "Block":
-            var block = Block()
-              ..size = Vector2(spawn.width, spawn.height)
-              ..position = Vector2(spawn.x, spawn.y);
-            _blockList.add(block);
-            add(block);
-            break;
-        }
-      }
+@override
+  KeyEventResult onKeyEvent(RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    if(keysPressed.contains(LogicalKeyboardKey.keyA)) {
+        spineboy.animationState.setAnimationByName(0, 'jump', true);
     }
-    add(map);
-    return super.onLoad();
-  }
-
-  @override
-  void update(double dt) {
-    _player.position.x += _player.velocity.x * _player.moveSpeed * dt;
-
-    checkHorizontalCollision();
-    if (_player.hasJumb && _player.isOnBlock) {
-      _player.onJump();
+    if(keysPressed.contains(LogicalKeyboardKey.keyW)) {
+        spineboy.animationState.setAnimationByName(0, 'shoot', true);
     }
-    _player.velocity.y += _player.gravity;
-    _player.position.y += _player.velocity.y * dt;
-
-    checkVerticalCollision();
-    super.update(dt);
-  }
-
-  void checkHorizontalCollision() {
-    for (var block in _blockList) {
-      if (checkCollision(_player, block)) {
-        if (_player.velocity.x > 0) {
-          _player.velocity.x = 0;
-          _player.position.x = block.position.x - _player.width;
-          break;
-        }
-        if (_player.velocity.x < 0) {
-          _player.velocity.x = 0;
-          _player.position.x = block.position.x + _player.width + block.width;
-          break;
-        }
-      }
+    if(keysPressed.contains(LogicalKeyboardKey.keyS)) {
+        spineboy.animationState.setAnimationByName(0, 'run', true);
     }
-  }
-
-  void checkVerticalCollision() {
-    for (var block in _blockList) {
-      if (checkCollision(_player, block)) {
-        if (_player.velocity.y > 0) {
-          _player.velocity.y = 0;
-          _player.position.y = block.position.y - _player.height;
-          _player.isOnBlock = true;
-          break;
-        }
-      }
+    if(keysPressed.contains(LogicalKeyboardKey.keyD)) {
+        spineboy.animationState.setAnimationByName(0, 'walk', true);
     }
-  }
-
-  bool checkCollision(player, block) {
-    double bw = block.width;
-    double bh = block.height;
-    double bx = block.position.x;
-    double by = block.position.y;
-
-    double pw = player.width;
-    double ph = player.height;
-    double psx = player.scale.x;
-    double px = psx > 0 ? player.position.x : player.position.x - pw;
-    double py = player.position.y;
-    return px + pw > bx && px < bx + bw && py + ph > by && py < by + bh;
-  }
-}
-
-class Block extends PositionComponent {
-  @override
-  FutureOr<void> onLoad() {
-    priority = 2;
-    debugMode = true;
-    return super.onLoad();
-  }
-}
-
-class Player extends SpriteAnimationGroupComponent
-    with HasGameRef<GameCtrl>, KeyboardHandler {
-  late final SpriteAnimation jumpCharacter;
-  MoveDirection moveDirection = MoveDirection.right;
-  double moveSpeed = 120;
-  double jumpForce = 320;
-  final int gravity = 10;
-  Vector2 velocity = Vector2.zero();
-
-  bool isFlipHorizontal = false;
-  bool hasJumb = false;
-  bool isOnBlock = false;
-
-  Player({required position}) : super(position: position);
-
-  @override
-  FutureOr<void> onLoad() async {
-    priority = 1;
-    debugMode = true;
-    jumpCharacter = SpriteAnimation.fromFrameData(
-        await Flame.images.load("Main Characters/Virtual Guy/Run (32x32).png"),
-        SpriteAnimationData.sequenced(
-            amount: 11, stepTime: 0.05, textureSize: Vector2.all(32)));
-    animations = {PlayerState.jump: jumpCharacter};
-    current = PlayerState.jump;
-    return super.onLoad();
-  }
-
-  void onJump() {
-    velocity.y = -jumpForce;
-    isOnBlock = false;
-    hasJumb = false;
-  }
-
-  @override
-  bool onKeyEvent(RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
-    if (keysPressed.contains(LogicalKeyboardKey.keyA) ||
-        keysPressed.contains(LogicalKeyboardKey.arrowLeft)) {
-      velocity.x = -1;
-    } else if (keysPressed.contains(LogicalKeyboardKey.keyD) ||
-        keysPressed.contains(LogicalKeyboardKey.arrowRight)) {
-      velocity.x = 1;
-    } else if (keysPressed.contains(LogicalKeyboardKey.space)) {
-      hasJumb = true;
-    } else {
-      velocity.x = 0;
-    }
+    // TODO: implement onKeyEvent
     return super.onKeyEvent(event, keysPressed);
   }
 
-  @override
-  void update(double dt) {
-    // applyGravity(dt);
-    updateMovement();
-    super.update(dt);
-  }
-
-  void applyGravity(dt) {
-    velocity.y += 0.5 * gravity;
-    position.y += velocity.y * dt;
-  }
-
-  void updateMovement() {
-    if (velocity.x < 0 && !isFlipHorizontal) {
-      flipHorizontallyAroundCenter();
-      isFlipHorizontal = true;
-    }
-    if (velocity.x > 0 && isFlipHorizontal) {
-      flipHorizontallyAroundCenter();
-      isFlipHorizontal = false;
-    }
-  }
+ @override
+ void onDetach() {
+  // Dispose the native resources that have been loaded for spineboy.
+  spineboy.dispose();
+ }
 }
